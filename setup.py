49e14599863675ede61eca5b9f4d69a442f10c55
Rafael Cunha de Almeida <almeidaraf@gmail.com>
2020-06-16 15:12:02 +0100

    Create conda package

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+from distutils.core import setup
+
+import yaml
+
+
+with open("conda/meta.yaml") as fp:
+    meta = yaml.load(fp)
+    version = meta["package"]["version"]
+
 
 setup(
    name='data_pipeline_api',
-   version='0.0.1',
+   version=version,
    description='Interacts with SCRC data pipeline',
    author='SCRC',
    author_email='scrc@glasgow.ac.uk',
    packages=['data_pipeline_api'],
-#   install_requires=['h5py', 'toml'], # handle by conda, somehow?
 )
