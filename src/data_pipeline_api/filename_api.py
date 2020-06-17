0941ddec364e9b747a92395a6ec7b3f4b406512a
Chris Hughes <chris.hughes@breedandcraft.com>
2020-06-17 18:07:17 +0100

    Developed the SCRC-specific parts.

--- src/data_pipeline_api/filename_api.py
+++ src/data_pipeline_api/filename_api.py
@@ -8,56 +8,60 @@ import toml
 from typing import Iterator, NamedTuple, Mapping, Iterable, Dict, Optional, Callable
 from pathlib import Path
 
-Identifier = Mapping[str, str]
+
 Metadata = Mapping[str, str]
-Paths = Mapping[Path, Metadata]
+PathMetadata = Mapping[Path, Metadata]
 
 
 class FilenameAPI:
-    def __init__(self, path: Path):
-        self._path = path
-        with open(self._path / ".metadata") as metadata_file:
-            self._metadata = {
+    """Load a toml-formatted file containing path metadata.
+
+    The format is of the toml file is expected to be sections of the form:
+
+        [path]
+        key = value
+        ...
+    
+    Where the metadata is defined by the (key, value) pairs.
+    """
+
+    def __init__(self, data_path: Path):
+        self._data_path = data_path
+        with open(self._data_path / ".metadata") as path_metadata_file:
+            self._path_metadata = {
                 Path(path): metadata
-                for path, metadata in toml.load(metadata_file).items()
+                for path, metadata in toml.load(path_metadata_file).items()
             }
-        with open(self._path / "config.toml") as config_file:
-            self._config = toml.load(config_file)
 
-    def find_matching(self, identifier: Identifier) -> Paths:
-        return {
-            path: metadata
-            for path, metadata in self._metadata.items()
-            if all(metadata.get(key) == value for key, value in identifier.items())
-        }
-
-    def match(self, identifier: Identifier) -> Paths:
-        # 0. begin with the input
-        # 1. use the config to augment our info
-        identifier = self.augment(identifier)
-        # 2. infer any other attributes we can (?)
-        # 3. look up everything that matches (?)
-        matching_filenames = self.find_matching(identifier)
-        # 4. filter using additional criteria (?)
-        # 5. if there is exactly one result, return it, otherwise fail
-        return matching_filenames
-
-    def augment(self, identifier: Identifier) -> Identifier:
-        """Augment an identifier using the configuration file using the "quantity"
-        attribute. Has no effect if "quantity" is not specified.
+    def find(self, metadata: Metadata) -> PathMetadata:
+        """Find PathMetadata corresponding to metadata.
         """
-        if "quantity" in identifier:
-            quantity = identifier["quantity"]
-            if quantity in self._config:
-                augmented_identifier = identifier.copy()
-                augmented_identifier.update(self._config[quantity])
-                return augmented_identifier
-        return identifier
-
-    def standard_filename(self, identifier: Identifier) -> Path:
-        if "quantity" not in identifier:
-            raise ValueError(f"{identifier} has no quantity")
-        return Path(identifier["quantity"]) / "{run_id}.{extension}".format(
-            **identifier
+        transformed_metadata = self.transform(metadata)
+        return self.filter(
+            {
+                self._data_path / path: metadata
+                for path, metadata in self._path_metadata.items()
+                if all(
+                    key in metadata and metadata[key] == value
+                    for key, value in transformed_metadata.items()
+                )
+            }
         )
+
+    def path(self, metadata: Metadata) -> Path:
+        """Generate a standard Path for metadata.
+        """
+        raise NotImplementedError
+
+    def transform(self, metadata: Metadata) -> Metadata:
+        """Transform metadata.
+        """
+        # FilenameAPI does no metadata transformation.
+        return metadata
+
+    def filter(self, path_metadata: PathMetadata) -> PathMetadata:
+        """Filter path_metadata.
+        """
+        # FilenameAPI does no path_metadata filtering.
+        return path_metadata
 
