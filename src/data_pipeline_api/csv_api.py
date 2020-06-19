226f029c742ddbcc2b5c724fb35d1b7714ccee4b
Chris Hughes <chris.hughes@breedandcraft.com>
2020-06-19 17:05:39 +0100

    Further development of the config and metadata handling.

--- src/data_pipeline_api/csv_api.py
+++ src/data_pipeline_api/csv_api.py
@@ -5,9 +5,9 @@ from data_pipeline_api.file_api import FileAPI
 
 class CsvAPI(FileAPI):
     def read_csv(self, quantity: str) -> DataFrame:
-        with TextIOWrapper(self.read(quantity=quantity, format="csv")) as csv_file:
+        with TextIOWrapper(self.read(quantity=quantity, extension="csv")) as csv_file:
             return read_csv(csv_file)
 
     def write_csv(self, quantity: str, value: DataFrame):
-        with TextIOWrapper(self.write(quantity=quantity, run_id=1, extension="csv")) as csv_file:
+        with TextIOWrapper(self.write(quantity=quantity, extension="csv")) as csv_file:
             value.to_csv(csv_file, index=False)
