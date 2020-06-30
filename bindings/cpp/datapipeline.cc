8f6d66367735c9bc9b541ddda0e4bbb0290ba1d0
qingfengxia <qingfeng.xia@ukaea.uk>
2020-06-30 16:52:48 +0100

    add read_table() from hdf5 file and test passed

--- bindings/cpp/datapipeline.cc
+++ bindings/cpp/datapipeline.cc
@@ -13,19 +13,21 @@ DataPipeline::DataPipeline(const string &config_file)
   // TODO: tidy up these variable names
   pd = py::module::import("pandas");
 
+  // TODO: StandardAPI needs here
   SimpleNetworkSimAPI = py::module::import(
-    "data_pipeline_api.simple_network_sim_api").attr("SimpleNetworkSimAPI");
+                            "data_pipeline_api.simple_network_sim_api")
+                            .attr("SimpleNetworkSimAPI");
 
-  api = SimpleNetworkSimAPI("repos/data_pipeline_api/examples/test_data_2/config.yaml");
+  api = SimpleNetworkSimAPI(config_file);
 
   py::object StandardAPIClass = py::module::import("data_pipeline_api.standard_api").attr("StandardAPI");
 
-  StandardAPI = StandardAPIClass("repos/data_pipeline_api/examples/test_data_2/config.yaml");
+  StandardAPI = StandardAPIClass(config_file);
 }
 
 double DataPipeline::read_estimate(string data_product, const string &component)
 {
-  double est = py::float_ (StandardAPI.attr("read_estimate")(data_product));
+  double est = py::float_(StandardAPI.attr("read_estimate")(data_product));
   // TODO: what about component?
   return est;
 }
@@ -47,44 +49,63 @@ double DataPipeline::read_sample(const string *data_product, const string &compo
 
 Table DataPipeline::read_table(const string &data_product)
 {
-//  using namespace pyglobals;
+  //  using namespace pyglobals;
 
   Table table;
 
+  // pandas is cable to read HDF5, but need extra parameter, component
+  // need test the file format
   py::object dataframe = api.attr("read_table")(data_product);
 
   vector<string> colnames = dataframe.attr("columns").attr("tolist")().cast<vector<string>>();
 
-  for (const auto &colname: colnames) {
-    
+  for (const auto &colname : colnames)
+  {
+
     string dtype = py::str(dataframe.attr("dtypes").attr(colname.c_str()));
 
-    if (dtype == "float64") {
+    if (dtype == "float64")
+    {
       vector<double> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<double>>();
       table.add_column<double>(colname, values);
-    } else {
+    }
+    else if (dtype == "int64")
+    {
+      vector<int64_t> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<int64_t>>();
+      table.add_column<int64_t>(colname, values);
+    }
+    else if (dtype == "bool")
+    {
+      vector<bool> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<bool>>();
+      table.add_column<bool>(colname, values);
+    }
+    /// TODO: this probably not working for std::string
+    // if (dtype == "object")
+    // {
+    //   vector<std::string> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<std::string>>();
+    //   table.add_column<std::string>(colname, values);
+    // }
+    else
+    {
       cout << "WARNING: Converting column " << colname << " from unsupported type " << dtype << " to string" << endl;
 
-      vector<string> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<string>>();
-      table.add_column<string>(colname, values);
+      //vector<string> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<string>>();
+      //table.add_column<string>(colname, values);
     }
   }
 
   return table;
 }
 
-//void DataPipeline::write_array(const string &data_product, const string &component, vector<double> array);
-// TODO: need to decide on a class for arrays.  Will return as vector of doubles for now
-
 // void DataPipeline::write_table(const string &data_product, const string &component,
 //                                const Table &table)
 // {
-//   map<string,vector<double>>  estc_map; // pybind automatically recognises a map as a dict
+//   map<string, vector<double>> estc_map; // pybind automatically recognises a map as a dict
 
-//   estc_map["a"] = vector<double>{1,2};
-//   estc_map["b"] = vector<double>{3,4};
+//   estc_map["a"] = vector<double>{1, 2};
+//   estc_map["b"] = vector<double>{3, 4};
 
 //   py::object estc_df = pd.attr("DataFrame")(estc_map);
 
-//   api.attr("write_table")("human/estimatec", estc_df);
+//   api.attr("write_table")(data_product, estc_df);
 // }
--- bindings/cpp/test_datapipeline.cc
+++ bindings/cpp/test_datapipeline.cc
@@ -1,23 +1,125 @@
 #include <iostream>
 
 #include "pybind11/embed.h"
+#include "pybind11/numpy.h"
+#include "pybind11/stl.h"
+namespace py = pybind11;
+using namespace pybind11::literals;
 
 #include "table.hh"
 #include "datapipeline.hh"
 
 using namespace std;
 
+const std::string TEST_HDF5_FILENAME = "test_table.h5";
+const std::string TEST_DATASET_NAME = "table_ds";
+
+/// hdf5 impl can add attributes/metadata while csv can not
+void write_table_metadata_to_hdf5(const Table &table)
+{
+  // read back and append
+  py::module h5py = py::module::import("h5py");
+  auto h5file = h5py.attr("File")(TEST_HDF5_FILENAME, "w"); // a+ mode?
+  auto dataset = h5file.attr("__getitem__")(TEST_DATASET_NAME);
+  py::object attrs = dataset.attr("attrs");
+  //attrs.attr("__getitem__")(py::str("units"));
+}
+
+/// todo:  copy these 2 functions back to datapipeline.cc after testing
+Table read_table(const string &data_product, const string &component)
+{
+  //  using namespace pyglobals;
+
+  Table table;
+
+  // pandas is cable to read HDF5, but need extra parameter, component
+  // need test the file format
+  //py::object dataframe = api.attr("read_table")(data_product);
+  py::module pd = py::module::import("pandas");
+  py::object dataframe = pd.attr("read_hdf")(data_product, component);
+
+  vector<string> colnames = dataframe.attr("columns").attr("tolist")().cast<vector<string>>();
+
+  for (const auto &colname : colnames)
+  {
+
+    string dtype = py::str(dataframe.attr("dtypes").attr(colname.c_str()));
+
+    if (dtype == "float64")
+    {
+      vector<double> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<double>>();
+      table.add_column<double>(colname, values);
+    }
+    else if (dtype == "int64")
+    {
+      vector<int64_t> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<int64_t>>();
+      table.add_column<int64_t>(colname, values);
+    }
+    else if (dtype == "bool")
+    {
+      vector<bool> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<bool>>();
+      table.add_column<bool>(colname, values);
+    }
+    /// TODO: this probably not working for std::string
+    // if (dtype == "object")
+    // {
+    //   vector<std::string> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<std::string>>();
+    //   table.add_column<std::string>(colname, values);
+    // }
+    else
+    {
+      cout << "WARNING: Converting column " << colname << " from unsupported type " << dtype << " to string" << endl;
+
+      // this cast to string does not work, just skip the column
+      //vector<string> values = dataframe[colname.c_str()].attr("tolist")().cast<vector<string>>();
+      //table.add_column<string>(colname, values);
+    }
+  }
+
+  return table;
+}
+
+//void DataPipeline::write_array(const string &data_product, const string &component, vector<double> array);
+// TODO: need to decide on a class for arrays.  Will return as vector of doubles for now
+
+void write_table(const string &data_product, const string &component,
+                 const Table &table)
+{
+  map<string, vector<double>> estc_map; // pybind automatically recognises a map as a dict
+
+  estc_map["a"] = vector<double>{1, 2};
+  estc_map["b"] = vector<double>{3, 4};
+
+  py::module pd = py::module::import("pandas");
+  py::object estc_df = pd.attr("DataFrame")(estc_map);
+  py::object dataframe = pd.attr("to_hdf")(data_product, component);
+  //api.attr("write_table")(data_product, estc_df);
+}
+
 int main()
 {
   pybind11::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
-  DataPipeline dp("repos/data_pipeline_api/examples/test_data_2/config.yaml");
+#if 0 // 
+  // todo: a better way to get example data from the repo
+  DataPipeline dp("../../../examples/test_data_2/config.yaml");
 
   // read_table
   Table table = dp.read_table("human/mixing-matrix");
-  cout << "human/mixing-matrix:" << endl << table.to_string() << endl;
+  cout << "human/mixing-matrix:" << endl
+       << table.to_string() << endl;
   vector<double> mixing = table.get_column<double>("mixing");
   cout << "mixing = [" << mixing.at(0) << "," << mixing.at(1) << ", ... ]" << endl;
+#endif
+
+  // python API does not support hdf5 read and write yet
+  py::exec("import pandas as pd\n"
+           "df = pd.DataFrame([[1, 1.1, True, 'str1'], [2, 2.2,  False, 'str2']], columns=['x', 'y', 'z', 's'])\n"
+           "df.to_hdf('" +
+           TEST_HDF5_FILENAME + "', '" + TEST_DATASET_NAME + "')\n");
+
+  Table h5table = read_table(TEST_HDF5_FILENAME, TEST_DATASET_NAME);
+  //write_table(TEST_HDF5_FILENAME, TEST_DATASET_NAME, h5table);
 
   return 0;
 }
