e4fe4bf04f8876ec35143cc2180251b73d4118d3
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-07-12 10:54:10 +0100

    cppbindings/tests.cc: Change REQUIRE to CHECK
    This ensures that all the assertions in a test are checked even if one fails

--- bindings/cpp/tests.cc
+++ bindings/cpp/tests.cc
@@ -15,26 +15,26 @@ const string CONFIG_FILE = "../../tests/data/config.yaml";
 
 TEST_CASE("read_estimate") {
   DataPipeline dp(CONFIG_FILE);
-  REQUIRE(dp.read_estimate("parameter", "example-estimate") == 1);
-  REQUIRE(dp.read_estimate("parameter", "example-distribution") == 2);
-  REQUIRE(dp.read_estimate("parameter", "example-samples") == 2);
+  CHECK(dp.read_estimate("parameter", "example-estimate") == 1);
+  CHECK(dp.read_estimate("parameter", "example-distribution") == 2);
+  CHECK(dp.read_estimate("parameter", "example-samples") == 2);
 }
 
 TEST_CASE("read_sample") {
   DataPipeline dp(CONFIG_FILE);
 
-  REQUIRE(dp.read_sample("parameter", "example-estimate") == 1);
+  CHECK(dp.read_sample("parameter", "example-estimate") == 1);
   pybind11::module::import("numpy.random").attr("seed")(0);
-  REQUIRE(dp.read_sample("parameter", "example-distribution") == 1.59174901632622);
-  REQUIRE(dp.read_sample("parameter", "example-samples") == 2);
+  CHECK(dp.read_sample("parameter", "example-distribution") == 1.59174901632622);
+  CHECK(dp.read_sample("parameter", "example-samples") == 2);
 }
 
 TEST_CASE("read_table") {
   DataPipeline dp(CONFIG_FILE);
   Table table = dp.read_table("object", "example-table");
 
-  REQUIRE(table.get_column<long>("a") == vector<long>{1,2});
-  REQUIRE(table.get_column<long>("b") == vector<long>{3,4});
+  CHECK(table.get_column<long>("a") == vector<long>{1,2});
+  CHECK(table.get_column<long>("b") == vector<long>{3,4});
 }
 
 TEST_CASE("table::get_column/types") {
@@ -42,6 +42,6 @@ TEST_CASE("table::get_column/types") {
 
   table.add_column<long>("a",{1,2,3});
 
-  REQUIRE(table.get_column<long>("a") == vector<long>{1,2,3});
-  REQUIRE_THROWS_AS(table.get_column<double>("a"), invalid_argument);
+  CHECK(table.get_column<long>("a") == vector<long>{1,2,3});
+  CHECK_THROWS_AS(table.get_column<double>("a"), invalid_argument);
 }
