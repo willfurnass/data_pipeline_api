cda8a4439438b0111a1e0cfd8240ff34f0b84ab9
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-07-12 11:50:42 +0100

    cppbindings/tests.cc: Add tests for remaining functionality

--- bindings/cpp/tests.cc
+++ bindings/cpp/tests.cc
@@ -13,11 +13,34 @@ using namespace std;
 
 const string CONFIG_FILE = "../../tests/data/config.yaml";
 
+TEST_CASE("write_estimate", "[!shouldfail]") {
+  DataPipeline dp(CONFIG_FILE);
+  CHECK_NOTHROW(dp.write_estimate("parameter", "example-estimate", 1.0));
+}
+
 TEST_CASE("read_estimate") {
   DataPipeline dp(CONFIG_FILE);
   CHECK(dp.read_estimate("parameter", "example-estimate") == 1);
   CHECK(dp.read_estimate("parameter", "example-distribution") == 2);
   CHECK(dp.read_estimate("parameter", "example-samples") == 2);
+}
+
+TEST_CASE("write_distribution", "[!shouldfail]") {
+  DataPipeline dp(CONFIG_FILE);
+  Distribution dist;
+  CHECK_NOTHROW(dp.write_distribution("parameter", "example-distribution", dist));
+}
+
+TEST_CASE("read_distribution","[!shouldfail]") {
+  DataPipeline dp(CONFIG_FILE);
+
+  CHECK_THROWS_AS(dp.read_distribution("parameter", "example-estimate"),
+                  pybind11::error_already_set);
+  CHECK(dp.read_distribution("parameter", "example-distribution").name == "gamma");
+  CHECK(dp.read_distribution("parameter", "example-distribution").params["shape"] == 1);
+  CHECK(dp.read_distribution("parameter", "example-distribution").params["scale"] == 2);
+  CHECK_THROWS_AS(dp.read_distribution("parameter", "example-samples"),
+                  pybind11::error_already_set);
 }
 
 TEST_CASE("read_sample") {
@@ -27,6 +50,11 @@ TEST_CASE("read_sample") {
   pybind11::module::import("numpy.random").attr("seed")(0);
   CHECK(dp.read_sample("parameter", "example-distribution") == 1.59174901632622);
   CHECK(dp.read_sample("parameter", "example-samples") == 2);
+}
+
+TEST_CASE("write_samples","[!shouldfail]") {
+  DataPipeline dp(CONFIG_FILE);
+  CHECK_NOTHROW(dp.write_samples("parameter", "example-samples", vector<double>{1,2,3}));
 }
 
 TEST_CASE("read_table") {
@@ -44,4 +72,27 @@ TEST_CASE("table::get_column/types") {
 
   CHECK(table.get_column<long>("a") == vector<long>{1,2,3});
   CHECK_THROWS_AS(table.get_column<double>("a"), invalid_argument);
+}
+
+TEST_CASE("read_array") {
+  DataPipeline dp(CONFIG_FILE);
+
+  Array<double> array = dp.read_array("object", "example-array");
+
+  CHECK(array(0) == 1);
+  CHECK(array(1) == 2);
+  CHECK(array(2) == 3);
+}
+
+TEST_CASE("write_array") {
+  DataPipeline dp(CONFIG_FILE);
+
+  Array<double> array(1,{3});
+
+  array(0) = 1;
+  array(1) = 2;
+  array(2) = 3;
+
+  // Get error "'memoryview' object has no attribute 'dtype'". 
+  CHECK_NOTHROW(dp.write_array("object", "example-array", array));
 }
