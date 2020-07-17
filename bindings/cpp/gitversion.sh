2d5dc80ea71632923120c75cbd47450c7cac0d38
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-07-17 20:23:27 +0100

    cppbindings/gitversion.sh: Store git URL

--- bindings/cpp/gitversion.sh
+++ bindings/cpp/gitversion.sh
@@ -8,12 +8,19 @@ tmpfile=${outfile}.tmp
 
 version=$(git describe --dirty --always)
 git_hash=$(git rev-parse HEAD)
+upstream_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+upstream_remote=${upstream_branch%/*}
+upstream_url=$(git remote get-url origin)
+
 echo "GIT ${version}"
 cat >$tmpfile <<EOF
 #pragma once
 #define GIT_VERSION "$version"
 #define GIT_HASH "$git_hash"
+#define GIT_URL "$upstream_url"
 EOF
+
+# TODO: probably want to convert the Git URL above from ssh to http
 
 if [ -r $outfile ]; then
     if diff -q $outfile $tmpfile >/dev/null; then
