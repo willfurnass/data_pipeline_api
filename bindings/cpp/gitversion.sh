3036bc08fc7c33c5e9ad5bd24ab7b23eb3afbdb9
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-08-01 01:14:34 +0100

    cppbindings/gitversion.sh: Quote strings

--- bindings/cpp/gitversion.sh
+++ bindings/cpp/gitversion.sh
@@ -10,7 +10,7 @@ version=$(git describe --dirty --always)
 git_hash=$(git rev-parse HEAD)
 upstream_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} || echo "NONE")
 upstream_remote=${upstream_branch%/*} || echo "NONE"
-upstream_url=$(git remote get-url ${upstream_remote} || echo "NONE")
+upstream_url=$(git remote get-url "${upstream_remote}" || echo "NONE")
 
 echo "GIT ${version}"
 cat >$tmpfile <<EOF
@@ -24,12 +24,12 @@ EOF
 
 # TODO: probably want to convert the Git URL above from ssh to http
 
-if [ -r $outfile ]; then
-    if diff -q $outfile $tmpfile >/dev/null; then
-        rm -f $tmpfile
+if [ -r "$outfile" ]; then
+    if diff -q "$outfile" "$tmpfile" >/dev/null; then
+        rm -f "$tmpfile"
         # No change to version
         exit 0
     fi
 fi
 
-mv $tmpfile $outfile
+mv "$tmpfile" "$outfile"
