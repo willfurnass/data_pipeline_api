3b4b30daf2aca6fa5d6c49f0ecfdeeb0be8b4cbb
Kristian Zarebski <kristian.zarebski@ukaea.uk>
2020-08-20 17:31:57 +0100

    Added Write Distribution Test

--- bindings/cpp/tests/test_distribution.cc
+++ bindings/cpp/tests/test_distribution.cc
@@ -8,3 +8,14 @@ TEST_F(SCRCAPITest, TestWriteDistribution)
     EXPECT_NO_FATAL_FAILURE(pDataPipeline_->read_distribution("output-parameter", "example-distribution").getParameter("k") == 10);
     EXPECT_NO_FATAL_FAILURE(pDataPipeline_->read_distribution("output-parameter", "example-distribution").getParameter("theta") == 10);
 }
+
+TEST_F(SCRCAPITest, TestReadDistribution)
+{
+  EXPECT_THROW(pDataPipeline_->read_distribution("parameter", "example-estimate"),
+                pybind11::error_already_set);
+  EXPECT_EQ(pDataPipeline_->read_distribution("parameter", "example-distribution").name, "gamma");
+  EXPECT_EQ(pDataPipeline_->read_distribution("parameter", "example-distribution").getParameter("k"), 1);
+  EXPECT_EQ(pDataPipeline_->read_distribution("parameter", "example-distribution").getParameter("theta"), 2);
+  EXPECT_THROW(pDataPipeline_->read_distribution("parameter", "example-samples"),
+                  pybind11::error_already_set);
+}
